a76b1f0ea40520f796b74f05e4e3445e510cac39
Rachel <rachelreyes2403@gmail.com>
2025-03-15 15:26:40 -0600

    cleaned up attempt at parallax for a saved version

--- reyes-portfolio/pages/index.tsx
+++ reyes-portfolio/pages/index.tsx
@@ -1,42 +1,53 @@
-// pages/index.tsx
-'use client';
+import Link from "next/link";
+import Image from "next/image";
 
-import Image from 'next/image';
-import { ParallaxBanner, ParallaxBannerLayer } from 'react-scroll-parallax';
-import ParallaxProviderWrapper from '../Components/ParallaxWrapper';
-
-const Home = () => {
+export default function Home() {
   return (
-    <ParallaxProviderWrapper>
-      <ParallaxBanner className="h-screen">
-        <ParallaxBannerLayer image="/Photos/88C71B99-4026-4BD4-8BB5-2838D395D929.jpg" speed={-20} />
-        <ParallaxBannerLayer speed={-10}>
-          <div className="absolute inset-0 flex items-center justify-center">
-            <h1 className="text-4xl font-bold text-white">Welcome to my personal website!</h1>
-          </div>
-        </ParallaxBannerLayer>
-      </ParallaxBanner>
+    //<div className={`grid grid-rows-[20px_1fr_20px] items-center min-h-screen p-8 pb-20 gap-16 `}>
+    <div className="pb-20 p-8">
+      <div className=" bg-[url('/Photos/88C71B99-4026-4BD4-8BB5-2838D395D929.jpg')] bg-fixed bg-cover bg-center min-h-screen flex flex-col items-center justify-center">
       <div className="bg-transparent/60">
         {/* Welcome Section */}
-        <div id="welcome-section" className="text-center p-10">
+        <div className="text-center p-10">
           <h1 className="text-4xl font-bold">Welcome to my personal website!</h1>
-          <p className="mt-2 text-2xl shadow">
-            Hi! My name is Rachel Reyes from Denver, Colorado, and I'm currently a senior in college. I'm looking for bigger opportunities to grow my software engineering skills after I graduate this year. This is a little about the outdoors, good reads, great food, fitness, and software engineering.
+          <p className="mt-2 text-2xl shadow ">
+            Hi! My name is Rachel Reyes from Denver, Colorado and currently a senior in college. Im looking for bigger opportunitie to grow my software engineering skills after I graduate this year.
+            This is little about the outdoors, good reads, great food, fitness, and
+            software engineering.
           </p>
         </div>
-        {/* Personal Images */}
-        <div id="middle-section" className="text-center flex flex-col justify-center items-center mt-4">
-          <h2 className="text-2xl font-semibold">This is me!</h2>
+
+       
+
+        {/* Personal Images and Videos */}
+        <div className="text-center flex flex-col justify-center items-center mt-4 n">
+          <h2 className="text-2xl font-semibold ">This is me!</h2>
+
           {/* Image 1 */}
           <div className="my-4 w-full flex justify-center">
             <Image
               src="/Photos/me.JPG"
-              width={320}
-              height={420}
+              width="320"
+              height="420"
               alt="Picture of me"
               className="rounded-lg shadow-lg"
             />
           </div>
+
+          {/* Video */}
+          <video
+            width="640"
+            height="360"
+            controls
+            className="my-4 rounded-lg shadow-lg"
+          >
+            <source
+              src="/Photos/export_1696555614633(1).mp4"
+              type="video/mp4"
+            />
+            Your browser does not support the video tag.
+          </video>
+
           {/* Image 2 */}
           <div className="my-4">
             <Image
@@ -47,8 +58,9 @@ const Home = () => {
               className="rounded-lg shadow-lg"
             />
           </div>
+
           {/* Image 3 */}
-          <div id="section4" className="my-4">
+          <div className="my-4">
             <Image
               src="/Photos/88C71B99-4026-4BD4-8BB5-2838D395D929.jpg"
               width={350}
@@ -56,39 +68,54 @@ const Home = () => {
               alt="Picture of a bridge with trees around it in Washington"
               className="rounded-lg shadow-lg"
             />
-          </div>
+        </div>
         </div>
       </div>
-      <footer className="py-10">
-        {/* Social Media Links */}
-        <div className="text-center">
-          <h2 className="text-2xl font-semibold">Follow Me on Social Media</h2>
-          <div className="mt-2 flex items-center space-x-2 justify-center">
-            <a
-              href="https://www.instagram.com/rachel.reyes24/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline"
-            >
-              Instagram
-            </a>
-            <svg
-              className="w-6 h-6 text-gray-800 dark:text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="currentColor"
-                fillRule="evenodd"
-                d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <a
-              href="https://www.linkedin.com/in/rachel-reyes2"
-              target="_blank"
-              rel="noopener noreferrer"
+      </div>
+      <footer className="py-10 ">
+      {/* Social Media Links */}
+            <div className="text-center">
+              <h2 className="text-2xl font-semibold">Follow Me on Social Media</h2>
+              <div className="mt-2 flex items-center space-x-2 justify-center">
+                  <Link
+                    href="https://www.instagram.com/rachel.reyes24/nk"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Instagram
+                  </Link>
+                  <svg
+                    className="w-6 h-6 text-gray-800 dark:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill="currentColor"
+                      fillRule="evenodd"
+                      d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+               
+                  <Link
+                    href="https://www.linkedin.com/in/rachel-reyes2"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    LinkedIn
+                  </Link>
+                  <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd" d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z" clip-rule="evenodd"/>
+                    <path d="M7.2 8.809H4V19.5h3.2V8.809Z"/>
+                  </svg>
+              </div>
+            </div>         
+
+      </footer>
+    </div>
+  );
+}
