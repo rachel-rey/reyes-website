f86e19ae75b69f95d90856aae27dc83371cad877
Rachel <rachelreyes2403@gmail.com>
2025-03-16 16:44:41 -0600

    fixed weird thing with navbar

--- Components/navbar.tsx
+++ Components/navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
   const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
-    <nav className="bg-navbar text-white shadow-md p-8">
+    <nav className="text-white shadow-md p-8">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
         <div className="text-xl font-bold">
@@ -44,7 +44,21 @@ export default function Navbar() {
 
       {/* Mobile Menu Overlay */}
       {isOpen && (
-        <div className="fixed inset-0 bg-navbar bg-opacity-95 flex flex-col items-center justify-center space-y-6 text-2xl z-50">
+        <div className="fixed inset-0 bg-black/80 backdrop-blur-lg flex flex-col items-center justify-center space-y-6 text-2xl z-50">
+          <button
+          onClick={toggleMenu}
+          className="md:hidden hover:text-yellow-900 text-white focus:outline-none focus:ring-2 focus:ring-yellow-900"
+        >
+          {isOpen ? (
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          ) : (
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+            </svg>
+          )}
+        </button>
           <Link href="/" className="hover:text-yellow-900" onClick={toggleMenu}>Home</Link>
           <Link href="/aboutMe" className="hover:text-yellow-900" onClick={toggleMenu}>About Me</Link>
           <Link href="/project" className="hover:text-yellow-900" onClick={toggleMenu}>Projects</Link>
