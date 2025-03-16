d7b9ba868443ad4be3eb338aac70d92f0a3fcb99
Rachel <rachelreyes2403@gmail.com>
2025-03-16 17:12:03 -0600

    added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from "next/app";
 import Navbar from "@/Components/navbar";
+import { Analytics } from "@vercel/analytics/react";
 import "@/styles/globals.css"; // Import your global styles if you have any
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Navbar />
+      <Analytics />
       <Component {...pageProps} />
     </>
   );
